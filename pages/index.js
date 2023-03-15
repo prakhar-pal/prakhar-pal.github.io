38b40d2568616e9a78450eb35b3e523fadbf7d33
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2023-03-16 00:50:53 +0530

    feat: add support for tailwind and remove index.html

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <h3>Work In Progress</h3>
+      <p>
+        Checkout <a href="/hit-the-circle">this</a> for the time being.
+      </p>
+    </>
+  );
+};
+
+export default Home;
