abeb08683646c4390fc1516387064a0d8cbe8c23
prakhar-pal <21095386+prakhar-pal@users.noreply.github.com>
2023-04-28 16:37:41 +0000

    Deploying to gh-pages from @ prakhar-pal/prakhar-pal.github.io@2060e18ff2b6788138a8c52aa9bf03e3ab9d771f 🚀

--- _next/static/pP1OMQlpjl4exWLIiCTS_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/de04418c3e0bcb1f.css","static/chunks/pages/index-ca48721ca8ef61f7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
